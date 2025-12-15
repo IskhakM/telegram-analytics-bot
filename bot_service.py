4b9106499e8d565189b64765ed58a2356d7f2bc1
IskhakM <murzaev48@gmail.com>
2025-12-15 11:09:14 +0600

    Удален доп ответ, теперь только точный ответ на вопрос

--- bot_service.py
+++ bot_service.py
@@ -13,7 +13,6 @@ from logging_config import configure_logger
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-
 BACKEND_URL = os.getenv('BACKEND_URL')
 
 logger = configure_logger(__name__)
@@ -22,9 +21,9 @@ router = Router()
 
 
 def check_tokens():
-    """Проверяет доступность необходимых переменных окружения."""
     tokens = {
         'TELEGRAM_BOT_TOKEN': TELEGRAM_TOKEN,
+        'BACKEND_URL': BACKEND_URL,
     }
 
     missing_tokens = [
@@ -38,9 +37,6 @@ def check_tokens():
 
 
 async def get_backend_answer(user_query):
-    """Отправляет запрос к API-сервису аналитики.
-    В случае успешного запроса возвращает словарь с ответом.
-    """
     try:
         async with httpx.AsyncClient() as client:
             response = await client.post(
@@ -56,61 +52,83 @@ async def get_backend_answer(user_query):
         raise ConnectionError(f'Бэкенд недоступен: {BACKEND_URL}')
 
     except httpx.HTTPStatusError as error:
-        logger.error(f'API вернул ошибку: {error.response.status_code}')
-        raise ValueError(f'Ошибка API: {error}')
+        status_code = error.response.status_code
+        
+        try:
+            detail = error.response.json().get('detail', 'Неизвестная ошибка API')
+        except:
+            detail = 'Неизвестная ошибка, API вернул не JSON'
+
+        logger.error(f'API вернул ошибку {status_code}: {detail}')
+        raise ValueError(f'Ошибка {status_code}: {detail}')
 
     except Exception as error:
-        logger.error(f'Ошибка при запросе к API: {error}')
+        logger.error(f'Непредвиденная ошибка при запросе к API: {error}')
         raise error
 
 
 @router.message(CommandStart())
 async def command_start_handler(message: types.Message):
-    """Ответ на команду /start."""
     logger.debug(f'Получена команда /start от user_id={message.from_user.id}')
     await message.answer(
-        "👋 Привет! Я — Аналитический Бот Text-to-SQL.\n"
-        "Спросите меня о статистике видео (просмотры, лайки, прирост).\n"
-        "Например: 'Сколько всего видео у креатора 1234?'"
+        '👋 Привет! Я — Аналитический Бот Text-to-SQL.\n'
+        'Спросите меня о статистике видео (просмотры, лайки, прирост).\n'
+        'Например: \'Сколько всего видео у креатора 1234?\''
     )
 
 
 @router.message()
 async def analyze_text_query(message: types.Message):
-    """Обрабатывает входящий текст, отправляет его в бэкенд и возвращает результат."""
     user_query = message.text
 
     if user_query.startswith('/'):
         return
 
-    logger.debug(f'Получен запрос: "{user_query}" от user_id={message.from_user.id}')
-    await message.answer("Анализирую запрос... 🧠")
+    logger.debug(
+        f'Получен запрос: \'{user_query}\' от user_id={message.from_user.id}'
+    )
 
     try:
         data = await get_backend_answer(user_query)
-        result = data.get("result")
+        result = data.get('result')
 
         if result is not None:
-            response_text = f"✅ Результат: **{result}**"
+            response_text = f'✅ Результат: **{result}**'
             logger.debug(f'Успешный ответ пользователю: {result}')
             await message.answer(response_text)
+            
         else:
             logger.warning('Бэкенд вернул пустой результат (None)')
-            await message.answer("❌ Не удалось получить числовой результат от бэкенда.")
+            await message.answer(
+                '❌ Не удалось получить числовой результат от бэкенда.'
+            )
 
     except ConnectionError:
         await message.answer(
-            "🛑 Ошибка подключения: Сервис аналитики недоступен.\n"
-            "Попробуйте позже."
+            '🛑 Ошибка подключения: Сервис аналитики недоступен.\n'
+            'Попробуйте позже.'
         )
+    except ValueError as error:
+        await message.answer(
+            f'❌ Ошибка анализа данных.\nПодробности: {error}'
+        )
+
     except Exception as error:
         logger.error(f'Сбой при обработке сообщения: {error}')
-        await message.answer("Произошла ошибка при выполнении анализа.")
+        await message.answer(
+            'Произошла непредвиденная ошибка при выполнении анализа.'
+        )
 
 
 async def main():
-    """Основная логика работы бота."""
     if not check_tokens():
+        sys.exit(1)
+
+    if not BACKEND_URL.startswith('http'):
+        logger.critical(
+            'BACKEND_URL должен начинаться с \'http://\' или'
+            '\'https://\'. Проверьте .env'
+        )
         sys.exit(1)
 
     bot = Bot(
@@ -122,7 +140,7 @@ async def main():
 
     await bot.delete_webhook(drop_pending_updates=True)
 
-    logger.info("🤖 Бот запущен. Ожидание сообщений...")
+    logger.info('🤖 Бот запущен. Ожидание сообщений...')
 
     try:
         await dp.start_polling(bot)
@@ -134,5 +152,5 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logger.info("Бот остановлен вручную.")
+        logger.info('Бот остановлен вручную.')
         sys.exit(0)
